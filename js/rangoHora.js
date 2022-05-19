01ede3a74bd904d8052f1571d0003b1debc96f03
Juan Marcos Benítez Navone Colomer <juanmarcosmega@gmail.com>
2022-05-19 17:10:07 +0200

    Selector de hora funcional

--- js/rangoHora.js
+++ js/rangoHora.js
@@ -1,1 +1,33 @@
-document.getElementById("horaMenor").value = document.getElementById("rango1").value
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 32400000,
+      max: 86400000,
+      values: [ 32400000, 86400000 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( convertMsToHM((ui.values[ 0 ])) + " - " + convertMsToHM((ui.values[ 1 ])));
+      }
+    });
+    $( "#amount" ).val( convertMsToHM($( "#slider-range" ).slider( "values", 0 )) +
+      " - " + convertMsToHM($( "#slider-range" ).slider( "values", 1 ) ));
+  } );
+
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+  function convertMsToHM(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+  
+    seconds = seconds % 60;
+    
+    minutes = seconds >= 30 ? minutes + 1 : minutes;
+  
+    minutes = minutes % 60;
+    hours = hours % 24;
+  
+    return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}`;
+
+  }
